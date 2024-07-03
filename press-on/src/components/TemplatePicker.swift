03725addaaa080c503245bb5d0be5233634a9a58
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-03 17:26:47 +0530

    Generated: `press-on/src/components/TemplatePicker.swift`

---
+++ press-on/src/components/TemplatePicker.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct TemplatePicker: View {
+    var body: some View {
+        Text("Template Picker View")
+    }
+}
+
+struct TemplatePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        TemplatePicker()
+    }
+}
