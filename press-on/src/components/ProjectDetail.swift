444181ed715ca675c3f7333c1b0ba0826dcc4be1
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-03 17:26:41 +0530

    Generated: `press-on/src/components/ProjectDetail.swift`

---
+++ press-on/src/components/ProjectDetail.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ProjectDetail: View {
+    var body: some View {
+        Text("Project Detail View")
+    }
+}
+
+struct ProjectDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectDetail()
+    }
+}
