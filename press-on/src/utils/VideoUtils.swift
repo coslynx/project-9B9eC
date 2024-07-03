fce2092be14ebe781ea29e8fd03adefdab7d27bf
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-03 17:27:01 +0530

    Generated: `press-on/src/utils/VideoUtils.swift`

---
+++ press-on/src/utils/VideoUtils.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+class VideoUtils {
+    
+    func addVideo(projectId: String, videoUrl: String) {
+        // Logic to add video to the project
+    }
+    
+    func editVideo(projectId: String, videoUrl: String) {
+        // Logic to edit video in the project
+    }
+    
+    func deleteVideo(projectId: String, videoUrl: String) {
+        // Logic to delete video from the project
+    }
+
+    func exportVideo(projectId: String, format: String) {
+        // Logic to export video in the specified format
+    }
+    
+    func previewVideo(projectId: String) {
+        // Logic to preview the video in the project
+    }
+    
+}
